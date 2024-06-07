526d9a3786a7e810a0d4f2cf046a50d100eb013c
tashkulov <nurislamtashkulov168@gmail.com>
2024-06-07 17:16:13 +0600

    added

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -58,7 +58,7 @@ const Form = () => {
         <div className={'form'}>
             <h3>Введите ваши данные </h3>
             <input className={'input'} type={'text'} value={country} onChange={onChangeCountry} placeholder={'Страна '}/>
-            <input className={'input'} type={'text'}value={street} onChange={onChangeStreet} placeholder={'Улица'}/>
+            <input className={'input'} type={'text'} value={street} onChange={onChangeStreet} placeholder={'Улица'}/>
 
             <select value={subject} onChange={onChangeSubject}  className={'select'}>
                 <option value={'physical'}>Физ.лицо</option>
@@ -69,58 +69,4 @@ const Form = () => {
         </div>
     );
 };
-
 export default Form;
-
-
-const TelegramBot = require('node-telegram-bot-api');
-
-const token ='7162104102:AAE2uwb6upTqfRFMLbtFDMGk79rIYSGRUNc'
-const webAppUrl='https://main--telegramwebsiteforme.netlify.app'
-
-const bot = new TelegramBot(token, {polling: true});
-
-bot.on('message', async (msg) => {
-
-    const chatIDd = msg.chat.id
-    const text = msg.text;
-
-    if (text === '/start') {
-        await bot.sendMessage(chatIDd, 'Ниже появиться кнопка ,заполи форму :)', {
-            reply_markup: {
-                keyboard: [
-                    [{text: 'Заполнить форму',web_app:{url:webAppUrl+'/form'}}]
-                ]
-            }
-        })
-
-
-        await bot.sendMessage(chatIDd, 'Заходи в наш интернет магазин по кнопке ниже', {
-            reply_markup: {
-                inline_keyboard: [
-                    [{text: 'Сделать заказ^^)',web_app:{url:webAppUrl}}]
-                ]
-            }
-        })
-    }
-
-
-    if(msg?.web_app_data?.data){
-        try {
-            const data=JSON.parse(msg.web_app_data?.data)
-            console.log(data)
-            await  bot.sendMessage(chatIDd,'Спасибо за обратную связь')
-            await bot.sendMessage(chatIDd,'Ваша страна:'+data?.country)
-            await bot.sendMessage(chatIDd,'Ваша улица:'+data?.street)
-
-            setTimeout(async ()=>{
-                await bot.sendMessage('Всю информацию вы получите в этом чате ')
-            },3000)
-        }catch (e){
-            console.log(e)
-        }
-
-
-    }
-
-});
